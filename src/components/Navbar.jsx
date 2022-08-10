81b20bbdd5055e6289612e3a7f1048cfad68aebc
Hasan <hasan.yilbat@gmail.com>
2022-08-10 16:31:15 +0300

    main

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          LA COLLECTION
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          More
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="products">
+                Products
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="d-none d-lg-block">
+          <button
+            type="button"
+            class="border border-2 border-dark btn btn-light"
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            class="border border-2 border-dark btn btn-light mx-2"
+          >
+            Register
+          </button>
+          <button
+            type="button"
+            class="border border-2 border-dark btn btn-light"
+          >
+            Cart
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "../pages/Cart";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Products from "../pages/Products";
+import Register from "../pages/Register";
+import Navbar from "../components/Navbar";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="products" element={<Products />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
